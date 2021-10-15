f11e882436df91f8548a3428495f2f8722529c0d
Dalius Slavickas <slavickasd@gmail.com>
2021-10-15 12:30:08 +0300

    init: Setup dev environment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.scss';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
