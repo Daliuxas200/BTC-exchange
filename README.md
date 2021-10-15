a7abe61a8ca2e6bd1bba3a3d10603cc2a32e8a06
Dalius Slavickas <slavickasd@gmail.com>
2021-10-15 20:58:37 +0300

    feat: finalised static HTML markup, styling

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # BTC exchange
+
+## Development Environment
+
+The development enviroment was set up using manual implementation of **Webpack**. This configuration provides some useful features nescesary for development yet remains simple enough to easily understand and adjust.
+
+1. **webpack-merge** was used to split the prod and dev modes into separate config files.
+2. **SCSS** was used, in combination with **mini-css-extract-plugin**, **css-loader**, **postcss-loader**, **sass-loader** and **autoprefixer** to generate a final minified and multibrowser compatible **CSS** file.
+3. **Webpack 5 asset module** was used to load font files.
+4. **html-webpack-plugin** was used to extract and set up the main index.html file with some main meta tags configured. **clean-webpack-plugin** for clearing old /dist files between builds.
+5. **Prettier** was used to help keep the code tidy
+
+#### To run the Dev server
+
+```
+npm run dev
+```
+
+#### To generate a final Build
+
+```
+npm run build
+```
+
+## Styling
+
+The Web Aplication was designed and its CSS programmed by me, only using **react-icons** library to get some SVG icons. I prefer writing my own styles as it provides a more lightweight package and allows for more creativity.
+
+Full power of SCSS was utilised including file imports, media queries, mixins and variables.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,87 @@
 import React from 'react';
+import { FaMinus, FaPlus, FaTimes, FaEquals } from 'react-icons/fa';
+import { HiRefresh } from 'react-icons/hi';
 
 const App = () => {
-  return <h1>BTC Exchange</h1>;
+  return (
+    <div className="page">
+      <div className="calculator">
+        <header className="calculator__header">
+          <h1 className="calculator__title">BTC Exchange</h1>
+          <span className="calculator__toggle">
+            <FaMinus />
+          </span>
+          <desc className="calculator__info">
+            Input amount in BTC and get converted amount in USD, GBP or EUR. Add
+            or remove currencies for your convenience. Conversion rate updated
+            every 60s.
+          </desc>
+        </header>
+        <main className="calculator__main">
+          <div className="calculator__main__left">
+            <div className="input">
+              <input
+                type="number"
+                className="input__field wrapper"
+                placeholder="Input amount"
+              />
+              <span className="input__indicator">BTC</span>
+            </div>
+            <div className="details wrapper">
+              <h3 className="details__title">BTC Exchange Rates</h3>
+              <h4 className="details__subtitle">
+                <span> Last updated 21:41 </span>
+                <HiRefresh />
+              </h4>
+              {/* <p className="details__pair">
+                <span> EUR </span> <span> 15.418465681</span>
+              </p>
+              <p className="details__pair">
+                <span> GBP </span> <span> 4615.4681</span>
+              </p>
+              <p className="details__pair">
+                <span> USD </span> <span> 465415.481</span>
+              </p> */}
+              <div className="loader">
+                <HiRefresh className="loader__icon" />
+              </div>
+            </div>
+          </div>
+          <div className="calculator__main__center">
+            <FaEquals />
+          </div>
+          <div className="calculator__main__right">
+            <ul className="results">
+              <li className="wrapper results__item">
+                <span>1,000,000$</span>
+                <span>GBP</span>
+                <FaTimes className="results__item__remove" />
+              </li>
+              <li className="wrapper results__item">
+                <span>1,000,000$</span>
+                <span>GBP</span>
+                <FaTimes className="results__item__remove" />
+              </li>
+              <li className="wrapper results__item">
+                <span>1,000,000$</span>
+                <span>GBP</span>
+                <FaTimes className="results__item__remove" />
+              </li>
+            </ul>
+            <div className="dropdown">
+              <button className="wrapper dropdown__button">
+                <FaPlus />
+              </button>
+              {/* <ul className="dropdown__list">
+                <li className="dropdown__list__item">USD</li>
+                <li className="dropdown__list__item">EUR</li>
+              </ul> */}
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default App;
